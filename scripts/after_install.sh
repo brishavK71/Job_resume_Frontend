b4fe327db45448e0ca880813ffa2be0c8caca0e6
brishavK71 <119672530+brishavK71@users.noreply.github.com>
2023-06-26 13:50:17 +0545

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 cd /home/ec2-user/server
+
+# Install peer dependencies
+npm install ajv@^8.0.0
+npm install typescript@^4.0.0
+
+# Install required packages
 npm install
-npm install --save react react-dom react-scripts react-particles-js
+
+# Fix vulnerabilities
+npm audit fix
+
+# Update deprecated package
+npm uninstall react-particles-js
+npm install --save react-tsparticles
+
+# Install PM2 globally
 npm install pm2 -g
